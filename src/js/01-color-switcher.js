297fbde88086c960e4d2a8c0a34026eacbc0269c
OOKucherenko <okld52dec@gmail.com>
2023-01-28 17:52:53 +0200

    end task1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,22 @@
+const ref = {
+  body: document.querySelector('body'),
+  btnStart: document.querySelector('button[data-start]'),
+  btnStop: document.querySelector('button[data-stop]'),
+};
+let timerId = null;
 
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+ref.btnStart.addEventListener('click', () => {
+  timerId = setInterval(() => {
+    ref.body.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+  ref.btnStart.setAttribute('disabled', 'true');
+});
+
+ref.btnStop.addEventListener('click', () => {
+  clearInterval(timerId);
+  ref.btnStart.removeAttribute('disabled');
+});
